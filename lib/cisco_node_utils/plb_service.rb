353c5977f5267e030a4e3b6ea3a45519e3dd3572
sai chintalapudi <saichint@cisco.com>
2017-01-23 16:19:45 -0800

    fix nil to empty

--- lib/cisco_node_utils/plb_service.rb
+++ lib/cisco_node_utils/plb_service.rb
@@ -192,12 +192,15 @@ module Cisco
     # so translate back to the input format
     def ingress_interface
       list = config_get('plb_service', 'ingress_interface', @get_args)
-      list.each do |intf, _next_hop|
+      rlist = []
+      list.each do |intf, next_hop|
         intf.downcase!
         intf.gsub!('eth', 'ethernet')
         intf.gsub!('po', 'port-channel')
+        next_hop = '' if next_hop.nil?
+        rlist << [intf, next_hop]
       end
-      list
+      rlist
     end
 
     def ingress_interface_cleanup
